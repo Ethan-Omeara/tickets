c3b8004e4e46f473dc0afb0a251bb2d4285b1571
EO4537 <ethan.omeara@hotmail.com>
2024-05-23 19:58:46 +1200

    Ticket Class

--- main.py
+++ main.py
@@ -20,4 +20,38 @@ info = {
     },
 }
 
+orders = []
+
+class Ticket:
+    def __init__(self) -> None:
+        self.Adults = 0
+        self.Children = 0
+        self.Concession = 0
+    
+    def __str__(self) -> str:
+        msg = f'''Tickets:
+Adults: {self.Adults}
+Children: {self.Children}
+Concession: {self.Concession}'''
+        return msg
+    
+    def update_tickets(self) -> None:
+        self.Adults = info["Adult"]["Input"].get()
+        self.Children = info["Child"]["Input"].get()
+        self.Concession = info["Concession"]["Input"].get()
+        print(info["Concession"]["Input"].get())
+    
+    def total_price(self) -> int:
+        total = 0
+        total += self.Adults * info["Adult"]["Price"]
+        total += self.Children * info["Child"]["Price"]
+        total += self.Concession * info["Concession"]["Price"]
+        return total
+    
+    def save_order(self) -> None:
+        orders.append(self)
+        info["Adult"]["Total"] += self.Adults
+        info["Child"]["Total"] += self.Children
+        info["Concession"]["Total"] += self.Concession
+
 root.mainloop()
